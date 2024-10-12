2d06c76fc6bd021df21318bfdebc4b6a52ac06bf
scaperow <scaperow@Macmini.lan>
2024-10-12 20:38:32 +0800

    for review

--- pages/index/index.ts
+++ pages/index/index.ts
@@ -2,9 +2,13 @@ const app = getApp()
 
 Page({
   data: {
+    h5PageUrl: decodeURIComponent('https://aibazi.scaperow.com'),
+    isLoaded: false
+  },
+  bindload(res:any) {
+    this.setData({
+      isLoaded: true
+    })
+  }
+});
 
-  },
-  onLoad: function () {
-    console.log('Welcome to Mini Code')
-  },
-})
